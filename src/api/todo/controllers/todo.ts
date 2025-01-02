8b13fa6ccc2a9a524113a51d7800541b4b7a169a
Aman Panwar <amanpanwar1672@gmail.com>
2025-01-02 19:41:08 +0530

    some api's

--- src/api/todo/controllers/todo.ts
+++ src/api/todo/controllers/todo.ts
@@ -4,4 +4,28 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::todo.todo');
+export default factories.createCoreController('api::todo.todo',({strapi})=>({
+    async create(ctx){
+        try{
+            const {todo_id,Todo_name,Todo_status,Todo_detail} = ctx.request.body;
+            const todo = await strapi.entityService.create('api::todo.todo',{
+                data:{todo_id,Todo_name,Todo_status,Todo_detail},
+            });
+            ctx.body = todo;
+        }catch(err){
+            ctx.throw(400,err);
+        }
+    },
+    async findOne(ctx){
+        try{
+            const {id} = ctx.params;
+            const todo = await strapi.entityService.findOne('api::todo.todo',id);
+            if(!todo){
+                return ctx.notFound('Todo not found');
+            }
+            ctx.body = todo;
+        }catch(err){
+            ctx.throw(400,err);
+        }
+    }
+}));
--- src/api/todo/routes/todo.ts
+++ src/api/todo/routes/todo.ts
@@ -4,4 +4,29 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::todo.todo');
+// export default factories.createCoreRouter('api::todo.todo',{
+//     only:['create'],
+//     config:{
+//         create:{
+//             auth:false,
+//             middlewares:[],
+//             policies:[],
+//         },
+//     },
+    
+// });
+export default {
+    routes:[
+        {
+            method:'POST',
+            path:'/todos',
+            handler:'todo.create' /* this route will exectute the function
+             created on controllers that is create */
+        },
+        {
+            method:'GET',
+            path:'/todos/:id',
+            handler:'todo.findOne'
+        }
+    ]
+}
